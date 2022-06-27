a160ba0a2a007722beb772899d2abc44cfb8e1e2
Raluca Ceuca <ceuca.raluca@gmail.com>
2022-06-27 06:47:16 +0300

    Initial Commit

---
+++ students-api/Controllers/StudentController.cs
@@ -0,0 +1,61 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using students_api.Data;
+ 
+namespace students_api.Controllers;
+ 
+[ApiController]
+[Route("[controller]")]
+public class StudentController: ControllerBase
+{
+    private readonly AppDbContext _studentDbContext;
+    public StudentController(AppDbContext studentDbContext)
+    {
+        _studentDbContext = studentDbContext;
+    }
+
+[HttpGet]
+public async Task<IActionResult> Get()
+{
+	var students = await _studentDbContext.Student.ToListAsync();
+	return Ok(students);
+}
+
+[HttpPost]
+public async Task<IActionResult> Post(students_api.Data.Entities.Student payload)
+{
+	_studentDbContext.Student.Add(payload);
+	await _studentDbContext.SaveChangesAsync();
+	return Ok(payload);
+}
+
+[HttpPut]
+public async Task<IActionResult> Put(students_api.Data.Entities.Student payload)
+{
+	_studentDbContext.Student.Update(payload);
+	await _studentDbContext.SaveChangesAsync();
+	return Ok(payload);
+}
+
+[HttpDelete]
+public async Task<IActionResult> Delete(int id)
+{
+	var studentToDelete = await _studentDbContext.Student.FindAsync(id);
+	if (studentToDelete == null)
+	{
+		return NotFound();
+	}
+	_studentDbContext.Student.Remove(studentToDelete);
+	await _studentDbContext.SaveChangesAsync();
+	return Ok();
+}
+
+}
